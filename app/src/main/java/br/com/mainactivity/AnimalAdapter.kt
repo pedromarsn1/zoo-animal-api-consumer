7cd84ec097bbe544ffc4d5a25cbf92cca60beaa8
Pedro Nascimento <pedromarsn@gmail.com>
2022-11-28 23:06:44 -0300

    re-doing projects

---
+++ app/src/main/java/br/com/mainactivity/AnimalAdapter.kt
@@ -0,0 +1,6 @@
+package br.com.mainactivity
+
+import br.com.mainactivity.model.Animal
+
+class AnimalAdapter(private val animal: ArrayList<Animal>) {
+}
---
+++ app/src/main/java/br/com/mainactivity/AnimalItem.kt
@@ -0,0 +1,11 @@
+package br.com.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimalItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.animal_item)
+    }
+}
